5780503b7428695b39a5f403a322799bbf7ae784
Jyah <24597915+Jyah@users.noreply.github.com>
2018-12-25 23:50:43 -0500

    add opencv basic drawing

---
+++ opencv_basicDrawing/CMakeFiles/basicDrawing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basicDrawing.dir/basicDrawing.cpp.o"
+  "basicDrawing.pdb"
+  "basicDrawing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basicDrawing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
