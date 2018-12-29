79972703b149ff280eb961998e1eefe3bc953107
Jyah <24597915+Jyah@users.noreply.github.com>
2018-12-28 23:57:36 -0500

    add histogram model

---
+++ opencv_backProjection/CMakeFiles/backProj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backProj.dir/backProject.cpp.o"
+  "backProj.pdb"
+  "backProj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/backProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
