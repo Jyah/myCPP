010e0e8b4829ea0580a63a821333035dfc745db7
Jyah <24597915+Jyah@users.noreply.github.com>
2018-12-28 23:45:55 -0500

    add sober gradient

---
+++ opencv_soberGradient/CMakeFiles/soberGradient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/soberGradient.dir/soberGradient.cpp.o"
+  "soberGradient.pdb"
+  "soberGradient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/soberGradient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
