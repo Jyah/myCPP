4db4a207b156001a819d3b29f0c59841fad44bac
Jyah <24597915+Jyah@users.noreply.github.com>
2018-12-31 22:13:50 -0700

    load and display image with openCV

---
+++ opencv_loadDisplay/CMakeFiles/loadDisplay.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loadDisplay.dir/loadDisplay.cpp.o"
+  "loadDisplay.pdb"
+  "loadDisplay"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loadDisplay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
