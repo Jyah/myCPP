28356a7cad9d9f5f855cd216467a52a5183aa7d3
Jyah <24597915+Jyah@users.noreply.github.com>
2019-01-01 17:31:17 -0700

    modify and save image

---
+++ opencv_modifySave/CMakeFiles/modifySave.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modifySave.dir/modifySave.cpp.o"
+  "modifySave.pdb"
+  "modifySave"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/modifySave.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
