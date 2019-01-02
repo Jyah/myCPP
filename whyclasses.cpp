fb0e33c82654828d29cac36b9e1a73e10f79e850
jyah <24597915+Jyah@users.noreply.github.com>
2019-01-02 16:31:50 -0700

    add playground main

---
+++ whyclasses.cpp
@@ -0,0 +1,19 @@
+/*Goal: sort students by multiple data vaule
+**
+**student1: Name: Joe Lime, ID#15, Grad. Date: 2019
+**student2: Name: Bob Green, ID#3, Grad. Date: 2020
+**student3: Name: SallyAnne Green , ID#1, Grad. Date: 2017
+**student4: Name: Annie Blue, ID#10, Grad. Date: 2020
+**student5: Name: Jose Lemon, ID#25, Grad. Date: 2016
+*/
+
+#include"whyclasses.hpp"
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    //ToDo sort the students by name, then by number, then by gradDate
+    return 0;
+}
