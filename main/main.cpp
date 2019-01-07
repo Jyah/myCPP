a7286baf7dac64a1ceaef9b71916768f6a6e1d6b
Jyah <24597915+Jyah@users.noreply.github.com>
2019-01-06 23:11:56 -0700

    add video player

--- main/main.cpp
+++ main/main.cpp
@@ -1,36 +1,21 @@
-/*Find the min and max and average of 15 integers that a user will input.
-**We will do it now for practice and again when we learn arrays. So you do not have to **keep all fifteen numbers stored in memory.
-*/
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
-#include "header.hpp"
+using namespace std;
 
-int main()
-{
-    int userInput = 0;
-    int maxNumber = 0;
-    int minNumber = 100;
-    int sumTotal = 0;
-    float average = 0; 
+int main( int argc, char** argv ) {
 
-    //get the numbers from the user
-    for(int i = 0; i < 15; i++)
-    {
-        std::cout << "Enter a number: ";
-        std::cin>>userInput;
-        std::cout << userInput << "\n";
-        if(userInput > maxNumber)
-        {
-            maxNumber = userInput;
-        }
-        if(userInput < minNumber)
-        {
-            minNumber = userInput;
-        }
-        sumTotal = sumTotal + userInput;
-    }
-    std::cout << "Maximum number = " << maxNumber << "\n";
-    std::cout << "Minimum number = " << minNumber << "\n";
-    average = sumTotal / 15.0;
-    std::cout << "Average = " << average << "\n";
-    return 0;
+  cv::namedWindow( "Example 2-3", cv::WINDOW_AUTOSIZE );
+  cv::VideoCapture cap;
+  cap.open( string(argv[1]) );
+
+  cv::Mat frame;
+  for(;;) {
+    cap >> frame;
+    if( frame.empty() ) break;             // Ran out of film
+    cv::imshow( "Example 2-3", frame );
+    if( (char) cv::waitKey(33) >= 0 ) break;
+  }
+
+  return 0;
 }
