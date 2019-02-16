62b21a96935cd74635e64ac6d0b1b282e50c6fe9
Jyah <24597915+Jyah@users.noreply.github.com>
2019-02-15 23:14:52 -0700

    project euler problem 3 in cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+int isPrime(int n)
+{
+	if(n<=3)
+	{return true;}
+	else if((n%2==0)||(n%3==0))
+	{
+		return 0;
+	}
+	int i = 5;
+	while(i*i<=n)
+	{
+		if((n%i==0)||(n%(i+2)==0))
+		{
+			return 0;
+		}
+		i=i+6;
+	}
+	return 1;
+}
+
 int main()
 {
-	int N = 4e6;
-	int a0 = 1;
-	int a1 = 2;
-	int a2 = a0+a1;
-	int s = a1;
-	while(a2<=N)
+	const long N = 600851475143;
+	long m = 2;
+	for(long n = 2;n<sqrt(N);n++)
 	{
-		a0 = a1;
-		a1 = a2;
-		a2 = a0+a1;
-		if(a2%2==0)
-		{
-			s = s+a2;
-		}
+		if(N%n==0)
+				{
+			if(isPrime(n))
+					{
+				if(n>m){m = n;cout<<m<<"\n";}
+					}
+				}
 	}
-	cout<<s<<"\n";
+	cout<<"The largest prime factor of " << N << " is " <<m<< "\n";
 	return 0;
 }
+
